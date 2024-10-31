c8108bf597c6d55794e314161b35da2f757a3288
João Denadai <165399053+JoaoDenadai@users.noreply.github.com>
2024-10-31 16:22:04 -0300

    Adicionado o arquivo dependencies.sh
    Adicionado o arquivo de dependencies.sh
    
    Utilize esse arquivo para efetuar a instalação das dependências de nodejs.

---
+++ dependencies.sh
@@ -0,0 +1,7 @@
+#Utilize esse arquivo para instalar as dependências de desenvolvimento
+#
+#Criado por João Denadai
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+nvm install 21
+npm install electron --save-dev
+npm install electron-builder --save-dev
