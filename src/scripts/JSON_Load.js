82c1a124b85c16920c62e1edaf3a3f2c3d55b9c1
João Denadai <165399053+JoaoDenadai@users.noreply.github.com>
2024-11-18 17:17:51 -0300

    Update JSON_Load.js
    Comentado o arquivo JSON_Load.js

--- src/scripts/JSON_Load.js
+++ src/scripts/JSON_Load.js
@@ -1,21 +1,35 @@
+// JSON_Load.js
+//
+// Arquivo criado para carregar o arquivo 'config.json'.
+//
+// Criado por João Denadai.
+
+//Importando módulo de manipulação de arquivos.
 const fs = require('fs');
+//Definindo uma constante. O nome de arquivo de configuração é 'config.json'.
 const filename = 'config.json';
 
+//Carrega as informações do arquivo JSON.
 function Load_settings_JSON()
 {
     return JSON.parse(fs.readFileSync(filename, 'utf8'));
 };
 
+//Chama a função que retorna o valor para uma variável.
+//O Loaded_Settings é a variável que contém os dados do arquivo JSON.
 let Loaded_Settings = Load_settings_JSON();
 
+//Função que carrega o tema do programa.
 function applyTheme()
 {
+    //Armazena o body da página.
     const body = document.body;
 
+    //Ao inicializar, ele irá remover todos os temas disponíveis, deixando sem informações no CSS.
     body.classList.remove("theme-branco", "theme-escuro", "ensolarado", "aurora-pastel", "carmesim-delicado");
 
-
-        switch(Loaded_Settings.window.theme)
+    //De acordo com o que estiver no arquivo de configurações, ele irá alterar o tema.
+    switch(Loaded_Settings.window.theme)
         {
             case "1":
                 body.classList.add("theme-branco");
@@ -38,9 +52,13 @@ function applyTheme()
         }
 };
 
+//Se no JSON, os logs estiverem desativados, ele irá desativar os logs de eventos no sistema.
+//É bom para deixar o programa mais leve.
 if(!Loaded_Settings.logs.isEnabled)
 {
+    //Desativa o campo de logs.
     document.getElementById('A7-Ariranha-Logs-element').disabled = true;
 }
 
+//Aplica o tema ao carregar a página.
 applyTheme();
