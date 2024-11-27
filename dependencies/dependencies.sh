8e1ed7f7a2d16e141919f3c4d8d89d0a3ed4769a
João Denadai <165399053+JoaoDenadai@users.noreply.github.com>
2024-11-27 15:54:04 -0300

    Update dependencies.sh
    Atualizada as dependências necessárias para o programa.

--- dependencies/dependencies.sh
+++ dependencies/dependencies.sh
@@ -1,10 +1,12 @@
 #Utilize esse arquivo para instalar as dependências de desenvolvimento
 #
 #Criado por João Denadai
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-nvm install 21
+cd ..
 npm install electron --save-dev
 npm install electron-builder --save-dev
 npm install electron-store
 npm install electron-logs
 npm install electron-updater
+npm install electron-updater@latest --save
+npm install typescript @types/node @types/electron --save-dev
+npm install typescript@latest --save-dev
