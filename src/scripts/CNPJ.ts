71ab01af39b1f6d3e00f1a2666bf61de1d60098f
João Denadai <165399053+JoaoDenadai@users.noreply.github.com>
2024-12-10 12:26:11 -0300

    Incluso novo arquivo src
    Arquivo src com novas funcionalidades.
    v1.13.1

---
+++ src/scripts/CNPJ.ts
@@ -0,0 +1,221 @@
+//CNPJ.js
+//
+//Arquivo de lógica de funcionamento para a manipulação do CNPJ.
+//
+//Criado por: João Denadai
+import {throwNewLog, throwException} from './Logs.js';
+
+
+//Inclusão de elementos do "../Index.html"
+//Evitar alterar o nome das variáveis.
+let semConversaoCNPJ:string, comConversaoCNPJ:string = "";
+
+
+//Globais
+const CNPJ_Input = document.getElementById('CNPJ-Input-element') as HTMLInputElement;
+const CNPJ_AutomaticFormat_CheckBox = document.getElementById('CNPJ-Checkbox-autoformat-element') as HTMLInputElement;
+const CNPJ_AutomaticCopy_CheckBox = document.getElementById('CNPJ-Checkbox-autocopy-element') as HTMLInputElement;
+let isCNPJ_AutomaticFormat_Checked:boolean = false;
+throwNewLog("Definindo valor inicial de constante global", "isCNPJ_AutomaticFormat_Checked", "false");
+let isCNPJ_AutomaticCopy_Checked:boolean = false;
+throwNewLog("Definindo valor inicial de constante global", "isCNPJ_AutomaticCopy_Checked", "false");
+
+
+//Botões da tela.
+const CNPJ_FormatButton = document.getElementById('CNPJ-Format-Button') as HTMLInputElement;
+const CNPJ_Unformat_Button = document.getElementById('CNPJ-Unformat-Button') as HTMLElement;
+const CNPJ_Copy_Button = document.getElementById('CNPJ-Copy-Button') as HTMLInputElement;
+
+
+//Verifica se a caixa de seleção "Formatar automáticamente" está habilitada.
+//Caso esteja, ele irá alterar a variável "isCNPJ_AutomaticFormat_Checked" para true.
+//Se não estiver, é falso.
+CNPJ_AutomaticFormat_CheckBox.addEventListener('change', function():void {
+    if(CNPJ_AutomaticFormat_CheckBox.checked)
+        {
+            //Caso estiver habilitado, ele irá:
+            //1. Vai alterar o valor da variavel "isCNPJ_AutomaticFormat_Checked" para true.
+            isCNPJ_AutomaticFormat_Checked = true;
+
+            //2.Desativar o botão de formatar, já que não é mais necessário.
+            CNPJ_FormatButton.disabled = true;
+
+            //3. Atualizar a formatação do CNPJ.
+            updateInputFormatation(false);
+
+            //4. Lançar um log de alteração na chave.
+            throwNewLog("Chave alterada.", "CNPJ_AutomaticFormat_CheckBox", "true");
+        } else
+        {
+            //Caso estiver desabilitado, ele irá:
+            //1. Vai alterar o valor da variavel "isCNPJ_AutomaticFormat_Checked" para false.
+            isCNPJ_AutomaticFormat_Checked = false;
+
+            //2. Ativar o botão de formatação manual.
+            CNPJ_FormatButton.disabled = false;
+
+            //3. Atualizar a formatação do CNPJ.
+            updateInputFormatation(false);
+
+            //4. Lança um log.
+            throwNewLog("Chave alterada.", "CNPJ_AutomaticFormat_CheckBox", "false");
+        }
+});
+
+//Um lister de eventos que habilita copiar o CNPJ automaticamente assim que ele for formatado.
+CNPJ_AutomaticCopy_CheckBox.addEventListener('change', () => {
+    if(CNPJ_AutomaticCopy_CheckBox.checked)
+    {
+        //Ele irá habilitar o "isCNPJ_AutomaticCopy_Checked" alterando o valor para "true".
+        isCNPJ_AutomaticCopy_Checked = true;
+
+        //Lança um log.
+        throwNewLog("Chave alterada.", "CNPJ_AutomaticCopy_CheckBox", "true");
+    }
+    else 
+    {
+        //Ele irá habilitar o "isCNPJ_AutomaticCopy_Checked" alterando o valor para "false".
+        isCNPJ_AutomaticCopy_Checked = false;
+
+        //Lança um log.
+        throwNewLog("Chave alterada.", "CNPJ_AutomaticCopy_CheckBox", "true");
+    }
+})
+
+//Um listener onde irá ser executado toda a vez que um dígito for incluso no campo de CNPJ
+CNPJ_Input.addEventListener('input', function():void {
+    //Se o CNPJ conter alguma formatação (Como pontos, barras ou traços) e menos de 14 digitos, ele irá remover a formatação do CNPJ.
+    //Uma medida de segurança para o sistema não bugar.
+    let CNPJ_element = document.getElementById("CNPJ-Input-element")as HTMLInputElement;
+    if(CNPJ_Input.value.includes(".") || CNPJ_Input.value.includes("/") || CNPJ_Input.value.includes("-") && CNPJ_Input.value.length < 14)
+    {
+        //Altera o tamanho máximo do input para 18.
+        //Serve para que o usuário consiga digitar ou alterar algum último dígito.
+        //UM CNPJ QUE POSSUI ".", "/" ou "-" SEMPRE VAI TER MAIS DE 14 DIGITOS.
+        
+        CNPJ_element.setAttribute("maxlength", "18");
+        throwNewLog("Atributo de 'maxlength' alterado.", "maxlength", "18");
+
+        //Remove a formatação.
+        removeFormatationFromCNPJ()
+    }
+    else
+    {
+        //Apenas ajusta o tamanho máximo do input para 14.
+        CNPJ_element.setAttribute("maxlength", "14");
+
+    }
+    //Atualiza a formatação do input.
+    //Está passando false pois não está habilitado o bypass da verificação se o CNPJ é válido.
+    updateInputFormatation(false);
+});
+
+
+//Ao apertar o botão de "formatar", ele irá chamar a função que formata o CNPJ.
+CNPJ_FormatButton.addEventListener('mousedown',function():void {
+    //Atualiza a informação do CNPJ.
+    //Está passando true pois ele bypassa a verificação de se o checkbox está aplicado.
+    updateInputFormatation(true);
+})
+
+//Ao apertar o botão de "desformatar", ele irá chamar a função que desformata o CNPJ.
+CNPJ_Unformat_Button.addEventListener('mousedown', function():void {
+    //Irá desformatar o CNPJ.
+    removeFormatationFromCNPJ();
+});
+
+//Ao apertar em copiar, ele irá chamar a função que copia o conteúdo do CNPJ para a área de transferência.
+CNPJ_Copy_Button.addEventListener('mousedown', function():void {
+    copyToClipboard(CNPJ_Input);
+})
+
+//Atualiza a informação de input.
+function updateInputFormatation(isEnabledCNPJ_AutomaticFormat_Bypass:boolean):void
+{
+    //Se a caixa de seleção estiver marcada ou o bypass estiver ativo...
+    if(isCNPJ_AutomaticFormat_Checked || isEnabledCNPJ_AutomaticFormat_Bypass)
+        {
+    
+            //...Se o CNPJ conter pontos ou barras ou traços ou for menor que 14 digitos...
+            if(CNPJ_Input.value.includes(".") || CNPJ_Input.value.includes("/") || CNPJ_Input.value.includes("-") || CNPJ_Input.value.length < 14)
+            {
+                //Se a verificação estiver com o bypass ativado e conter menos de 14 digitos, ele irá apresentar um erro no log de eventos.
+                if(CNPJ_Input.value.length < 14 && isEnabledCNPJ_AutomaticFormat_Bypass)
+                {
+                    //Lança uma exception.
+                    throwException("O CNPJ está incorreto, tente novamente.", "CNPJ_Input.value", CNPJ_Input.value);
+                }
+            }
+            //Senão...
+            else
+            {
+                //...Se o CNPJ tiver o tamanho de 14 digitos...
+                if(CNPJ_Input.value.length === 14)
+                    {
+                        //Salva o que tiver no Input do CNPJ não formatado na variável "semConversaoCNPJ".
+                        semConversaoCNPJ = CNPJ_Input.value;  
+    
+                        //Salva o que tiver no Input do CNPJ não formatado na variável "paraConverterCNPJ".
+                        //Informa o tamanho do CNPJ no console para fins de rastreio de execução.
+                        let paraConverterCNPJ:string = CNPJ_Input.value;
+                        
+                        //Divide os 14 digitos do CNPJ em partes de uma matriz.
+                        let partesDoCNPJ:string[] =
+                        [
+                            paraConverterCNPJ.slice(0, 2),
+                            paraConverterCNPJ.slice(2, 5),
+                            paraConverterCNPJ.slice(5, 8),
+                            paraConverterCNPJ.slice(8, 12),
+                            paraConverterCNPJ.slice(12, 14),
+                        ];
+    
+                        //Faz a conversão adicionando pontos, barras e traços ao CNPJ.
+                        comConversaoCNPJ = `${partesDoCNPJ[0]}.${partesDoCNPJ[1]}.${partesDoCNPJ[2]}/${partesDoCNPJ[3]}-${partesDoCNPJ[4]}`;
+
+                        //Adiciona o valor do CNPJ alterado diretamente no valor do Input.
+                        CNPJ_Input.value = comConversaoCNPJ;
+
+                        //Adiciona um novo log, informando que o CNPJ foi formatado com sucesso.
+                        throwNewLog("O CNPJ foi formatado!", "CNPJ_Input.value", CNPJ_Input.value);
+
+                        //Faz com que o bypass fique em falso.
+                        //É importante para que o bypass não fique ativado.
+                        //Dessa forma o sistema trata de maneira única cada formatação manual e automática.
+                        isEnabledCNPJ_AutomaticFormat_Bypass = false;
+
+                        //Se a cópia automática estiver ativada, ele copia automaticamente ao formatar.
+                        if(isCNPJ_AutomaticCopy_Checked)
+                        {
+                            copyToClipboard(CNPJ_Input);
+                        }
+                    };
+            }
+            
+        }
+};
+
+//Função que remove a formatação.
+function removeFormatationFromCNPJ():string
+{
+    //Remove todos os caracteres especiais do CNPJ e armazena em uma variável especial e temporária.
+    let removeFormatationCNPJtemp:string = CNPJ_Input.value.replace(/[.\-\/]/g, '');
+    //Retorna o valor ao input.
+    CNPJ_Input.value = removeFormatationCNPJtemp as string;
+    //Adiciona um log.
+    throwNewLog("Formatação removida!", "CNPJ_Input.value", CNPJ_Input.value);
+
+    //Caso chamem essa função, irá retornar o valor do CNPJ (Isso após efetuarmos a desformatação).
+    return CNPJ_Input.value;
+}
+
+//Função de copiar o conteúdo para a área de transferência.
+function copyToClipboard(input:any):void
+{
+    //Armazena o texto que desejamos copiar para uma variável.
+    let textToCopy:string = input.value;
+    //Copia para a área de transferência.
+    navigator.clipboard.writeText(textToCopy);
+
+    //Adiciona um novo log.
+    throwNewLog("Copiado para a área de transferência!", "textToCopy", textToCopy);
+}
---
+++ src/scripts/JSON_Load.ts
@@ -0,0 +1,82 @@
+// JSON_Load.js
+//
+// Arquivo criado para carregar o arquivo 'config.json'.
+//
+// Criado por João Denadai.
+
+//Importando módulo de manipulação de arquivos.
+const fs = require('fs');
+//Definindo uma constante. O nome de arquivo de configuração é 'config.json'.
+const filename:string = 'config.json';
+
+
+interface file {
+    window: {
+        isResizable: boolean,
+        theme: string,
+        devTools: boolean,
+        devMode: boolean,
+    },
+    logs: {
+        isEnabled: boolean,
+    }
+}
+
+//Carrega as informações do arquivo JSON.
+function Load_settings_JSON():file
+{
+    const JSONContent:file = JSON.parse(fs.readFileSync(filename, 'utf8'));
+    return JSONContent;
+};
+
+//Chama a função que retorna o valor para uma variável.
+//O Loaded_Settings é a variável que contém os dados do arquivo JSON.
+export let Loaded_Settings:file = Load_settings_JSON();
+
+//Função que carrega o tema do programa.
+function applyTheme():void
+{
+    //Armazena o body da página.
+    const body = document.body;
+
+    //Ao inicializar, ele irá remover todos os temas disponíveis, deixando sem informações no CSS.
+    body.classList.remove("theme-branco", "theme-escuro", "ensolarado", "aurora-pastel", "carmesim-delicado");
+    
+    //De acordo com o que estiver no arquivo de configurações, ele irá alterar o tema.
+    switch(Loaded_Settings.window.theme)
+        {
+            case "1":
+                body.classList.add("theme-branco");
+                break;
+            case "2":
+                body.classList.add("theme-escuro");
+                break;
+            case "3":
+                body.classList.add("ensolarado");
+                break;
+            case "4":
+                body.classList.add("aurora-pastel");
+                break;
+            case "5":
+                body.classList.add("carmesim-delicado");
+                break;
+            case "6":
+                body.classList.add("verde-menta");
+                break
+        }
+};
+
+//Se no JSON, os logs estiverem desativados, ele irá desativar os logs de eventos no sistema.
+//É bom para deixar o programa mais leve.
+if(!Loaded_Settings.logs.isEnabled)
+{
+    let logsElement = document.getElementById('A7-Ariranha-Logs-element') as HTMLInputElement;
+    //Desativa o campo de logs.
+    if(logsElement)
+    {
+        logsElement.disabled = true;
+    }
+}
+
+//Aplica o tema ao carregar a página.
+applyTheme();
